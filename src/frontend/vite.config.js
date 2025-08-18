7a77efaba03a8d1353fab6676371ed486fb9337b
tomy.thanh <tomy.thanh@votee.com>
2025-08-18 14:38:17 +0700

    fix: fix cicd

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -3,15 +3,26 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { loadEnv } from 'vite'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig((mode) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    define: Object.fromEntries(
+      Object.entries(env).map(([key, val]) => [
+        `import.meta.env.${key}`, JSON.stringify(val)
+      ])
+    ),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }
 })
